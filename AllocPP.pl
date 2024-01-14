6c837ddd01241d830d08da960b0c3a1cd7cd2870
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-15 02:54:17 +0330

    Added POD documentation to the script

--- AllocPP.pl
+++ AllocPP.pl
@@ -20,6 +20,86 @@ my @dumpfns;
 my $hash_function_name;
 my $hash_function;
 
+
+
+=head1 NAME
+
+AllocPP.pl - Perl script for generating static allocation heaps in C
+
+=head1 SYNOPSIS
+
+    AllocPP.pl [-i input_file] [-o output_file]
+
+=head1 DESCRIPTION
+
+This script scans a C file for specific directives and generates static allocation heaps based on those directives.
+
+The directives are in the style of C preprocessor directives. These directives, and their signature, are as follows:
+
+	#alloc <heap name> , <allocate function name> , <reallocate function name> , <dump function name>
+	#hashfunc <hash function name>
+
+The signature of the generated functions is:
+
+	void* <allocate function>(size_t size);
+	void* <reallocate function>(void* ptr, size_t resize);
+	void  <dump function>(void);
+
+All the directive parameters are mandatory. You can create several heaps, but only one '#hashfunc' directive is necessary.
+
+Basically, the #alloc directive creates an allocation heap, and an allocation, reallocation, and dump function for it. You can use this heap to allocate and reallocate data, and then dump it at the end.
+
+The '#hashfunc' directive names the MurMurHash function that is necessary for the process.
+
+Notice that the functions do safe allocations and reallocations; you do not need to check for errors and null pointers. If a returned pointer for calloc and realloc is NULL, it will error out and exit.
+
+Also, if the size given to realloc is smaller than the already-given size, it will warn the user.
+
+=head1 EXAMPLE
+
+Suppose you have a C file named "example.c" with the following content:
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    // Your existing C code here...
+
+    #alloc heap1, allocate1, reallocate1, dump1
+    #alloc heap2, allocate2, reallocate2, dump2
+    #hashfunc my_hash_function
+
+    int main() {
+        void* int ptr1 = allocate1(sizeof(int));
+        void* int ptr2 = allocate2(sizeof(int));
+
+        *ptr1 = 22;
+        *ptr2 = 33;
+
+        ptr1 = reallocate1(ptr1, sizeof(int) * 2);
+        *(ptr1 + sizeof(int)) = 44;
+
+        // do stuff
+
+        dump1();
+        dump2();
+    }
+ 
+=head1 OPTIONS
+
+=over 4
+
+=item B<-i>
+
+Input file. If not provided, it defaults to STDIN.
+
+=item B<-o>
+
+Output file. If not provided, it defaults to STDOUT.
+
+=back
+
+=cut
+
 while (<$input_file>) {
     if (/^#alloc\s+(.+)\s*,\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*,\s*([a-zA-Z_][a-zA-Z0-9_]*),\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*$/) {
         push @alloc_directives, {
@@ -36,7 +116,7 @@ while (<$input_file>) {
 }
 
 sub generate_pointer_hash_function {
-    my $random_integer = int(rand(100)) + 1; 
+    my $random_integer = int(rand(100)) + 1;
     $hash_function = <<"END_HASH_FUNCTION";
 unsigned long long $hash_function_name(void *ptr) {
     const unsigned long long m = 0xc6a4a7935bd1e995;
@@ -91,8 +171,8 @@ void *$alloc_directive->{allocfn_name}(unsigned long long size) {
     node->next = ___HEAP__$alloc_directive->{heap_name};
     void *alloc_error_check = calloc(1, size);
     if (alloc_error_check == NULL) {
-	fprintf(stderr, "Allocation error");
-	fputc(10, stderr);
+        fprintf(stderr, "Allocation error");
+        fputc(10, stderr);
     }
     node->alloc_data = calloc(1, size);
     node->alloc_size = size;
@@ -127,6 +207,7 @@ void *$alloc_directive->{reallocfn_name}(void *ptr, unsigned long long resize) {
                 exit(EXIT_FAILURE);
             } else {
                 node->alloc_data = alloc_error_check;
+		node->alloc_size = resize;
             }
             return alloc_error_check;
         }
@@ -186,4 +267,5 @@ print $output_file $hash_function, "\n\n";
 
 # Print notifications
 print $output_file "\n", "/* The preceding file was preprocessed with AllocPP.pl, a Perl script for generating static allocation heaps in C */", "\n\n";
+
 
